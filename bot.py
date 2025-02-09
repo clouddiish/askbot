37786d63ed6f995874b0a17d016c54a1473976f1
Olga Szymczak <olga.szymczaak@gmail.com>
2025-02-09 18:09:23 +0100

    feat: send a message to dc when someone joins mc server

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ from utils.mc_utils import *
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD_ID = int(os.getenv("GUILD_ID"))
+GAYMING_ID = int(os.getenv("GAYMING_ID"))
 
 bot = commands.Bot(intents=discord.Intents.default(), command_prefix="!")
 
@@ -27,10 +28,18 @@ async def set_current_mc_players_channels():
     guild = bot.get_guild(GUILD_ID)
     logger.info(f"guild gotten: {guild}")
 
+    gayming_channel = bot.get_channel(GAYMING_ID)
+
     mcserver_category = get_mcserver_category(guild)
+    mcserver_channels_set = get_mcserver_channels_set(mcserver_category)
+    mcserver_players_set = get_mcserver_players_set()
 
     logger.info("comparing mcserver channels set with mcserver players set")
-    if get_mcserver_channels_set(mcserver_category) != get_mcserver_players_set():
+    if mcserver_channels_set != mcserver_players_set:
+
+        if len(mcserver_channels_set) < len(mcserver_players_set):
+            await gayming_channel.send("Somebody joined the minecraft server!")
+
         logger.info("clearing mcserver channels")
         await clear_mcserver_channels(mcserver_category)
         logger.info("creating new mcserver channels")
